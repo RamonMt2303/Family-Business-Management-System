ada6b67050478f7b44dac0731bf3c1c42124d284
RamonMt2303 <69860663+RamonMt2303@users.noreply.github.com>
2020-11-19 22:15:08 -0600

    Add files via upload

---
+++ Empleados.h
@@ -0,0 +1,163 @@
+//
+//  Empleados.h
+//  proyecto A01702917
+//
+//  Created by Ramon Mtz on 19/11/20.
+//
+
+#ifndef Empleados_h
+#define Empleados_h
+#include <iostream>
+using namespace std;
+
+class Empleados{
+private:
+    float sueldo;
+    string nombre;
+    int edad;
+    string ocupacion;
+    string especialidad;
+    string productividad;
+    string preparacion;
+    string equipo;
+    int antiguedad;
+    string estadoCivil;
+public:
+    Empleados(){
+        this -> sueldo = 0;
+        this -> nombre = "";
+        this -> edad = 0;
+        this -> ocupacion = "";
+        this -> especialidad = "";
+        this -> productividad = "";
+        this -> preparacion = "";
+        this -> equipo = "";
+        this -> antiguedad = 0;
+        this -> estadoCivil = "";
+    }
+    float getSueldo(){
+        return this -> sueldo;
+    }
+    string getNombre(){
+        return this -> nombre;
+    }
+    int getEdad(){
+        return this -> edad;
+    }
+    string getOcupacion(){
+        return this -> ocupacion;
+    }
+    string getEspecialidad(){
+        return this -> especialidad;
+    }
+    string getProductividad(){
+        return this -> productividad;
+    }
+    string getPreparacion(){
+        return this -> preparacion;
+    }
+    string getEquipo(){
+        return this -> equipo;
+    }
+    int getAntiguedad(){
+        return this -> antiguedad;
+    }
+    string getEstadoCivil(){
+        return this -> estadoCivil;
+    }
+
+    void setSueldo(float sueldo){
+        this -> sueldo = sueldo;
+    }
+    void setNombre(string nombre){
+        this -> nombre = nombre;
+    }
+    void setEdad(int edad){
+        this -> edad = edad;
+    }
+    void setOcupacion(string ocupacion){
+        this -> ocupacion = ocupacion;
+    }
+    void setEspecialidad(string especialidad){
+        this -> especialidad = especialidad;
+    }
+    void setProductividad(string productividad){
+        this -> productividad = productividad;
+    }
+    void setPreparacion(string preparacion){
+        this -> preparacion = preparacion;
+    }
+    void setEquipo(string equipo){
+        this -> equipo = equipo;
+    }
+    void setAntiguedad(int antiguedad){
+        this -> antiguedad = antiguedad;
+    }
+    void setEstadoCivil(string estado){
+        this -> estadoCivil = estado;
+    }
+    
+    void llenarInfo(){
+        float sueldo = 0;
+        string nombre = "";
+        int edad = 0;
+        string ocupacion = "";
+        ocupacion = "";
+        string especialidad = "";
+        string productividad = "";
+        string preparacion = "";
+        string equipo = "";
+        int antiguedad = 0;
+        string estadoCivil = "";
+        cout << " ¿Cuál es el nombre del empleado? " << endl;
+        cin.ignore();
+        getline(cin, nombre);
+        setNombre(nombre);
+        cout << " ¿Cuál es su sueldo? " << endl;
+        cin >> sueldo;
+        setSueldo(sueldo);
+        cout << " ¿Cuál es su ocupación en el negocio? " << endl;
+        cin.ignore();
+        getline(cin, ocupacion);
+        setOcupacion(ocupacion);
+        cout << " ¿Cuál es su edad? " << endl;
+        cin.ignore();
+        cin >> edad;
+        setEdad(edad);
+        cout << " ¿Cuenta con alguna especialidad? (buen soldador, inciativa, buenas decisiones, etc) " << endl;
+        cin.ignore();
+        getline(cin,especialidad);
+        setEspecialidad(especialidad);
+        cout << " ¿Qué tan productivo es? (bajo, medio o alto) " << endl;
+        cin.ignore();
+        getline(cin, productividad);
+        setProductividad(productividad);
+        cout << " ¿Cuenta con algún nivel de preparación escolar? " << endl;
+        cin.ignore();
+        getline(cin, preparacion);
+        setPreparacion(preparacion);
+        cout << " ¿Sabe trabajar en equipo? " << endl;
+        cin.ignore();
+        getline(cin, equipo);
+        setEquipo(equipo);
+        cout << " ¿Cuánto lleva en el negocio? " << endl;
+        cin >> antiguedad;
+        setAntiguedad(antiguedad);
+        cout << " ¿Cuál es su estado civil (casado, soltero, viudo, divorciado, etc)? " << endl;
+        cin.ignore();
+        getline(cin, estadoCivil);
+        setEstadoCivil(estadoCivil);
+    }
+    void printInfo(){
+        cout << " El nombre del empleado es " << getNombre() << endl;
+        cout << " Su sueldo es de " << getSueldo() << endl;
+        cout << " Su ocupación es " << getOcupacion() << endl;
+        cout << " Su edad es " << getEdad() <<endl;
+        cout << " Su especialidad es " << getEspecialidad() << endl;
+        cout << " Su nivel de productividad es " << getProductividad() << endl;
+        cout << " Su nivel de preparación es " << getPreparacion() << endl;
+        cout << getNombre() << "" << getEquipo() << " sabe trabajar en equipo " << endl;
+        cout << " Sus años de antiguedad en la empresa son " << getAntiguedad() << "años" << endl;
+    }
+};
+#endif /* Empleados_h */
---
+++ Transporte.h
@@ -0,0 +1,112 @@
+//
+//  Tansporte.h
+//  proyecto A01702917
+//
+//  Created by Ramon Mtz on 19/11/20.
+//
+
+#ifndef Transporte_h
+#define Transporte_h
+#include <iostream>
+using namespace std;
+
+/*
+ Inicializo mi segunda clase, que va enfocada principalmente a los métodos de transporte que\ tiene mi papá dependiendo de la situación, cuenta con una grúa, camionetas de carga y\ descarga, e intenté hacerlo lo más semejante posible.
+ */
+class Transporte{
+private:
+    float gasolina;
+    string nombre;
+    string tipoVehiculo;
+    float capacidad;
+    string concepto;
+    float costoCombustible;
+    
+public:
+    Transporte(){
+        this -> gasolina = 0;
+        this -> nombre = "";
+        this -> tipoVehiculo = "";
+        this -> capacidad = 0;
+        this -> concepto = "";
+        this -> costoCombustible = 0;
+    }
+    float getGasolina(){
+        return this -> gasolina;
+    }
+    string getNombre(){
+        return this -> nombre;
+    }
+    string getTipoVehiculo(){
+        return this -> tipoVehiculo;
+    }
+    float getCapacidad(){
+        return this -> capacidad;
+    }
+    string getConcepto(){
+        return this -> concepto;
+    }
+    float getCostoCombustible(){
+        return this -> costoCombustible;
+    }
+    void setGasolina(float gasolina){
+        this -> gasolina = gasolina;
+    }
+    void setNombre(string nombre){
+        this -> nombre = nombre;
+    }
+    void setTipoVehiculo(string vehiculo){
+        this -> tipoVehiculo = vehiculo;
+    }
+    void setCapacidad(float capacidad){
+        this -> capacidad = capacidad;
+    }
+    void setConcepto(string concepto){
+        this -> concepto = concepto;
+    }
+    void setCostoCombustible(float costo){
+        this -> costoCombustible = costo;
+    }
+    
+    void printInfo(){
+        cout << " El vehículo es " <<getNombre() << endl;
+        cout << " El tipo del vehículo es " <<getTipoVehiculo() << endl;
+        cout << " El vehículo es capáz de cargar " <<getCapacidad() <<" kg " << endl;
+        cout << " El concepto del uso para este vehículo es " << getConcepto() << endl;
+        cout << " El vehículo tiene actualmente " <<getGasolina() <<" litros de gasolina" << endl;
+        cout << " El costo por cada km recorrido de combustible es de " << getCostoCombustible() << " pesos/km " << endl;
+        
+    }
+    void llenarInfo(){
+        string nombre = "";
+        float gasolina = 0;
+        string tipo = "";
+        float carga = 0;
+        string conceptoUso = "";
+        float costoCombustible = 0;
+        
+        cout << " ¿Cómo se llama el vehículo? " << endl;
+        cin.ignore();
+        getline(cin, nombre);
+        setNombre(nombre);
+        cout << " ¿De qué tipo es el vehículo? Carga pesada, transporte de personal, etc" << endl;
+        cin.ignore();
+        getline(cin, tipo);
+        setTipoVehiculo(tipo);
+        cout << " ¿Cuánto puede cargar aprox (en kg)? " << endl;
+        cin >> carga;
+        setCapacidad(carga);
+        cout << " ¿Cuál va a ser el concepto de uso del vehículo? (entregar material, recoger material, entregar productos terminados) " << endl;
+        cin.ignore();
+        getline(cin, conceptoUso);
+        setConcepto(conceptoUso);
+        cout << " ¿Cuánta gasolina tiene el vehículo? " << endl;
+        cin >> gasolina;
+        setGasolina(gasolina);
+        cout << " ¿Cuánto cuesta cada km de combustible recorrido? " << endl;
+        cin >> costoCombustible;
+        setCostoCombustible(costoCombustible);
+    }
+};
+
+#endif /* Transporte_h */
